76b82bb5ff07ae3377f342d5e4d6c4eba75e4627
dineshn9999 <38238150+dineshn9999@users.noreply.github.com>
2018-04-10 15:29:07 +1000

    Initial commit

---
+++ customer-service/src/main/java/io/eventuate/examples/tram/sagas/ordersandcustomers/customers/CustomersServiceMain.java
@@ -0,0 +1,36 @@
+package io.eventuate.examples.tram.sagas.ordersandcustomers.customers;
+
+import io.eventuate.examples.tram.sagas.ordersandcustomers.customers.web.CustomerWebConfiguration;
+import io.eventuate.jdbckafka.TramJdbcKafkaConfiguration;
+import io.eventuate.tram.commands.common.ChannelMapping;
+import io.eventuate.tram.commands.common.DefaultChannelMapping;
+import io.eventuate.tram.commands.producer.TramCommandProducerConfiguration;
+import io.eventuate.tram.events.publisher.TramEventsPublisherConfiguration;
+import io.eventuate.tram.sagas.orchestration.SagaOrchestratorConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@Configuration
+@Import({CustomerConfiguration.class,
+        CustomerWebConfiguration.class,
+        TramEventsPublisherConfiguration.class,
+        TramCommandProducerConfiguration.class,
+        SagaOrchestratorConfiguration.class,
+        TramJdbcKafkaConfiguration.class})
+@ComponentScan
+public class CustomersServiceMain {
+
+  @Bean
+  public ChannelMapping channelMapping() {
+    return DefaultChannelMapping.builder().build();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(CustomersServiceMain.class, args);
+  }
+}
